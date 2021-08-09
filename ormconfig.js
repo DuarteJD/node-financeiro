1e285650b5fc9657b14001a22eda0dadc12bd63e
Julio Duarte <jcduarte.ti@gmail.com>
2021-08-09 20:32:40 -0300

    Adicionado a conexão segura

--- ormconfig.js
+++ ormconfig.js
@@ -1,26 +1,26 @@
-console.log(process.env.DATABASE_URL);
-console.log(process.env.DEV_MODE);
-console.log(process.env.DEV_MODE === 1 ? 'src/entity/**/*.ts' : 'dist/entity/**/*.js');
-console.log("=-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-=");
 
 module.exports = {
-   "name" : "default",
-   "type": "postgres",
-   "url" : process.env.DATABASE_URL,
-   "synchronize": false,
-   "logging": false,
-   "entities": [
-      process.env.DEV_MODE === 1 ? 'src/entity/**/*.ts' : 'dist/src/entity/**/*.js'
-   ],
-   "migrations": [
+  "name" : "default",
+  "type": "postgres",
+  "url" : process.env.DATABASE_URL,
+  "synchronize": false,
+  "logging": false,
+  "extra": {
+    "ssl": true,
+    "rejectUnauthorized": false
+  },
+  "entities": [
+    process.env.DEV_MODE === 1 ? 'src/entity/**/*.ts' : 'dist/src/entity/**/*.js'
+  ],
+  "migrations": [
     process.env.DEV_MODE === 1 ? 'src/migration/**/*.ts' : 'dist/src/migration/**/*.js'
-   ],
-   "subscribers": [
+  ],
+  "subscribers": [
     process.env.DEV_MODE === 1 ? 'src/subscriber/**/*.ts' : 'dist/src/subscriber/**/*.js'
-   ],
-   "cli": {
-      "entitiesDir": "src/entity",
-      "migrationsDir": "src/migration",
-      "subscribersDir": "src/subscriber"
-   }
+  ],
+  "cli": {
+    "entitiesDir": "src/entity",
+    "migrationsDir": "src/migration",
+    "subscribersDir": "src/subscriber"
+  }
 }
